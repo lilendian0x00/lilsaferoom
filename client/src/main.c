f4db85792e0945f0603ba3aaf655eb2860c549ff
lilendian0x00 <arianth82@gmail.com>
2023-03-14 08:17:48 -0500

    init

---
+++ client/src/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdint.h>
+#include <stdbool.h>
+#include <sys/socket.h>
+
+
+int main(int argc, char *argv[]) {
+    return 0;
+}
